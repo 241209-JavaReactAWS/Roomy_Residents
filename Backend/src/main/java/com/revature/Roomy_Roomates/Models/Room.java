0ababa3573d01e8d9ebe58e415e840c3cc870740
Shane Duncan <ksdfjkfsdfkds@gmail.com>
2024-12-30 10:12:26 -0500

    Create Room.java

---
+++ Backend/src/main/java/com/revature/Roomy_Roomates/Models/Room.java
@@ -0,0 +1,36 @@
+package com.revature.Roomy_Roomates.entities;
+
+
+@Getter
+@Setter
+@Entity
+@Table(name = 'rooms')
+public class Rooms {
+
+    @Column(name = "room_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer room_id;
+
+    @Column(name = "hotel_id", nullable = false)
+    @ManyToOne(cascade = CascadeType.MERGE, fetch = FetchType.EAGER)
+    @JoinColumn(name = "hotel_id")
+    private Hotel hotel;
+
+    @Column(name = "room_type", nullable = false)
+    private String room_type;
+
+    @Column(name = "status", nullable = false)
+    private Status status;
+
+    @Column(name = "image", nullable = false)
+    private String image;
+
+    public Room() {}
+
+    public Rooms(int room_id, int hotel_id, String room_type, Status status, String image) {
+        this.room_id = room_id;
+        this.hotel_id = hotel_id;
+        this.room_type = room_type;
+        this.status = status;
+        this.image = image;
+    }
