87c21ce676dfe122ef4b7b0f977d7df212334ba6
mattm <kumkowskimateusz2002@gmail.com>
2024-12-30 09:38:28 -0500

    Initial Files for backend

---
+++ Backend/src/main/java/com/revature/Roomy_Roomates/RoomyRoomatesApplication.java
@@ -0,0 +1,13 @@
+package com.revature.Roomy_Roomates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomyRoomatesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomyRoomatesApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/revature/Roomy_Roomates/RoomyRoomatesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.Roomy_Roomates;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomyRoomatesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
