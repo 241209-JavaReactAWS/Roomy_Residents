75bd94975a4db67d09b52c66b8df239d03d1935f
Shane Duncan <ksdfjkfsdfkds@gmail.com>
2024-12-30 11:29:08 -0500

    rooms model import into DAO

--- Backend/src/main/java/com/revature/Roomy_Roomates/DAOs/RoomDAO.java
+++ Backend/src/main/java/com/revature/Roomy_Roomates/DAOs/RoomDAO.java
@@ -2,6 +2,7 @@ package com.revature.Roomy_Roomates.DAOs;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import com.revature.Roomy_Roomates.Models.Rooms;
 
 @Repostiory
 public interface RoomDAO extends JpaRepository<Room, Integer> {
