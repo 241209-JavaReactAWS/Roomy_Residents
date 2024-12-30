6846ffd418fc38109730466c3b8e822a22686312
mattm <kumkowskimateusz2002@gmail.com>
2024-12-30 10:04:45 -0500

    Created Owner and User Entities

--- Backend/src/main/java/com/revature/Roomy_Roomates/entities/Owner.java
+++ Backend/src/main/java/com/revature/Roomy_Roomates/entities/Owner.java
@@ -1,4 +1,53 @@
 package com.revature.Roomy_Roomates.entities;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "owners")
 public class Owner {
+
+    @Id
+    @Column(name = "ownerId")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer ownerId;
+    private String buisnessName;
+    private String buisnessEmail;
+    private Integer buisnessPhoneNumber;
+
+    public Owner() {
+    }
+
+    public Integer getOwnerId() {
+        return ownerId;
+    }
+
+    public void setOwnerId(Integer ownerId) {
+        this.ownerId = ownerId;
+    }
+
+    public String getBuisnessName() {
+        return buisnessName;
+    }
+
+    public void setBuisnessName(String buisnessName) {
+        this.buisnessName = buisnessName;
+    }
+
+    public String getBuisnessEmail() {
+        return buisnessEmail;
+    }
+
+    public void setBuisnessEmail(String buisnessEmail) {
+        this.buisnessEmail = buisnessEmail;
+    }
+
+    public Integer getBuisnessPhoneNumber() {
+        return buisnessPhoneNumber;
+    }
+
+    public void setBuisnessPhoneNumber(Integer buisnessPhoneNumber) {
+        this.buisnessPhoneNumber = buisnessPhoneNumber;
+    }
 }
+
+
--- Backend/src/main/java/com/revature/Roomy_Roomates/entities/User.java
+++ Backend/src/main/java/com/revature/Roomy_Roomates/entities/User.java
@@ -2,8 +2,92 @@ package com.revature.Roomy_Roomates.entities;
 
 import jakarta.persistence.*;
 
+import java.util.Date;
+
 @Entity
 @Table(name = "users")
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "userId")
+    private Integer userId;
+
+    private Integer ownerId = -1;
+
+    private String username;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    @Temporal(TemporalType.DATE)
+    private Date date;
+
+    public User() {
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public Integer getOwnerId() {
+        return ownerId;
+    }
+
+    public void setOwnerId(Integer ownerId) {
+        this.ownerId = ownerId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
 }
