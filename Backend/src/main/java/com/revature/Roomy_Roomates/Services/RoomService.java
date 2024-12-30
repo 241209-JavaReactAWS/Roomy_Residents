aebf90380f5e9aa71397fc773cb13072b83c8bef
Shane Duncan <ksdfjkfsdfkds@gmail.com>
2024-12-30 13:26:56 -0500

    roomservice file

---
+++ Backend/src/main/java/com/revature/Roomy_Roomates/Services/RoomService.java
@@ -0,0 +1,19 @@
+package com.revature.Roomy_Roomates.Services;
+
+import com.revature.Roomy_Roomates.DAOs.RoomDAO;
+import com.revature.Roomy_Roomates.Models.Room;
+import com.revature.Roomy_Roomates.Models.Room;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+
+
+@Service
+public class RoomService {
+
+}
