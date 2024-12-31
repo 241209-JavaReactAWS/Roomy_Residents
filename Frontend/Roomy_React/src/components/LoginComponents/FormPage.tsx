7ecc6f9447923df5d7cbd79c2697c5c5d03cbcdd
mattm <kumkowskimateusz2002@gmail.com>
2024-12-31 16:09:50 -0500

    Created base files

---
+++ Frontend/Roomy_React/src/components/LoginComponents/FormPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function FormPage() {
+  return (
+    <div className='formPage'>
+
+    </div>
+  )
+}
+
+export default FormPage
---
+++ Frontend/Roomy_React/src/components/LoginComponents/LoginPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function LoginPage() {
+  return (
+    <div className='LoginPage'>
+        <div id='LoginGraphic'></div>
+        <div className='loginForm'></div>
+    </div>
+  )
+}
+
+export default LoginPage
